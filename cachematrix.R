db6011267063b31356d5df3aa7410cba3140feaa
sahanabs <sahushridhar1995@gmail.com>
2016-07-16 21:29:34 +0530

    Matrix-Inverse Caching in R
    The contained source code is written in partial fulfillment of the course requirements as listed by R Programming - Coursera. 
    
    Computing the inverse of a matrix is an expensive task and caching the inverse of a matrix enhances efficiency of the underlying code.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following 2 functions cache the inverse of a matrix
 
-## Write a short comment describing this function
+## Creates a list to set a matrix, get a matrix, set the value of the mean and get the value of the mean
 
 makeCacheMatrix <- function(x = matrix()) {
-
+ i <- NULL
+ set <- function(y) {
+        x <<- y
+        i <<- NULL
+ }
+get <- function() x
+setinverse <- function(inverse) i <<- inverse 
+getinverse <- function() i
+list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Sets the value of the inverse of the matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        i <- x$getinverse()
+        if(!is.null(i)) {
+                message("getting data")
+                return(i)
+        }
+        mat <- x$get()
+        i <- solve(mat)
+        x$setinverse(i)
+        i
 }
